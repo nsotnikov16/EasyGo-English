fdfdea5bed4f6e5f8b56341237b1a8910bbfe06f
Nikita Sotnikov <nsotnikov16@yandex.ru>
2022-07-09 17:25:46 +0500

    update

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,8 +1,19 @@
 const swiperBanner = new Swiper('.banner .swiper', {
     loop: true,
-    
+
     pagination: {
         el: '.banner .swiper-pagination',
         clickable: true
     }
 })
+
+/* Спойлеры */
+const spoilers = document.querySelectorAll('.spoiler')
+if (spoilers.length > 0) {
+    spoilers.forEach(spoiler => {
+        const top = spoiler.querySelector('.spoiler__top')
+        top.addEventListener('click', () => {
+            spoiler.classList.toggle('spoiler_open')
+        })
+    })
+}
