e6f314796dcd8509c94f5ab91f3b04e81b0199c1
Nikita Sotnikov <nsotnikov16@yandex.ru>
2022-07-17 19:53:43 +0500

    Updates

--- js/app.js
+++ js/app.js
@@ -343,8 +343,8 @@ var t;
 function up() {
     var top = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
     if (top > 0) {
-        window.scrollBy(0, -100);
-        t = setTimeout('up()', 1);
+        window.scrollBy(0, ((top + 300) / -10));
+        t = setTimeout('up()', 20);
     } else clearTimeout(t);
     return false;
 }
